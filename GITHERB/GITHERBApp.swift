7548669edaf959c572d6b6bdd80495768cc32196
wxxd-fxrest <fxrest704@gmail.com>
2024-07-22 15:17:04 +0900

    [Feat] Add Launch Screen

--- GITHERB/GITHERBApp.swift
+++ GITHERB/GITHERBApp.swift
@@ -9,16 +9,19 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 @main
 struct GITHERBApp: App {
+    init() {
+        Thread.sleep(forTimeInterval: 2)
+    }
+    
     var body: some Scene {
         WindowGroup {
             AuthContentView()
