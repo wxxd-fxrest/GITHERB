6e1d9b0a3f511a34c8c76d3e25112e423ee847df
wxxd-fxrest <fxrest704@gmail.com>
2024-07-22 14:04:47 +0900

    [Start] Project start

---
+++ GITHERB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GITHERB
+//
+//  Created by 밀가루 on 7/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GITHERB/GITHERBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHERBApp.swift
+//  GITHERB
+//
+//  Created by 밀가루 on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GITHERBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
