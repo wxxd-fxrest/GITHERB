4642691aaf2ad15ed278cab0c0a064cce8e25830
wxxd-fxrest <fxrest704@gmail.com>
2024-07-22 15:40:17 +0900

    [Feat] Auth

--- GITHERB/View/AuthContentView.swift
+++ GITHERB/View/AuthContentView.swift
@@ -13,7 +13,7 @@ struct AuthContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello!")
+            Text("Auth)
         }
         .padding()
     }
